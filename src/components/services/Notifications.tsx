1492998a44c0289f4954f2eb5fafacb3c0c16bc2
sabinayadhakal <sabinayadhakal007@gmail.com>
2026-01-04 15:50:49 +0545

    final image commit

--- src/components/services/Notifications.tsx
+++ src/components/services/Notifications.tsx
@@ -12,7 +12,7 @@ interface NotificationsProps {
 export default function Notifications({ showTestToast = false, className }: NotificationsProps) {
   useEffect(() => {
     if (showTestToast) {
-      toast.success("Notifications initialized successfully!");
+      toast.success("");
     }
   }, [showTestToast]);
 
