a92250dafa2278087f1b7678476a63f23daa3a65
sabinayadhakal <sabinayadhakal007@gmail.com>
2026-03-09 14:31:55 +0545

    changed some pages from the header

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -434,13 +434,27 @@ const MobileServicesDropdown = ({ closeMenu }: { closeMenu: () => void }) => {
                 className="block py-3 px-4 text-white/80 hover:text-[#C5E0ED] hover:bg-[#153653]/40 rounded-lg transition-colors"
                 onClick={closeMenu}
               >
-                City Tours
+                Multi Day Cultural Tours
               </Link>
               <Link
                 href="/services/mountain-flight"
                 className="block py-3 px-4 text-white/80 hover:text-[#C5E0ED] hover:bg-[#153653]/40 rounded-lg transition-colors"
                 onClick={closeMenu}
               >
+                <Link
+                href="/services/trekking"
+                className="block py-3 px-4 text-white/80 hover:text-[#C5E0ED] hover:bg-[#153653]/40 rounded-lg transition-colors"
+                onClick={closeMenu}
+              >
+                Day Hikings
+              </Link>
+              <Link
+                href="/services/city-tours"
+                className="block py-3 px-4 text-white/80 hover:text-[#C5E0ED] hover:bg-[#153653]/40 rounded-lg transition-colors"
+                onClick={closeMenu}
+              >
+Day Sightseeings              </Link>
+              
                 Mountain Flights and Helicopter Sightseeings
               </Link>
               <Link
