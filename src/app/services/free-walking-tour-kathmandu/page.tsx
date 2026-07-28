582139924c0d28eb1d12fecb48c700d1c52bf78e
Sabinaya Dhakal <sabinayadhakal007@gmail.com>
2026-07-28 19:15:32 +0545

    just a test to check if this has worked

--- src/app/services/free-walking-tour-kathmandu/page.tsx
+++ src/app/services/free-walking-tour-kathmandu/page.tsx
@@ -63,7 +63,7 @@ const tourStops = [
     name: "Kathe Swoyambhu",
     icon: Church,
     description:
-      "A beautiful embodiment of religious harmony where Hindu, Buddhist and Tibetan cultures coexist. Small stupas with symbolic paintings surround the main stupa. Very photogenic, often undergoing gentle construction.",
+      "A embodiment of religious harmony where Hindu, Buddhist and Tibetan cultures coexist. Small stupas with symbolic paintings surround the main stupa. Very photogenic, often undergoing gentle construction.",
   },
   {
     name: "Local Lassi Stop",
